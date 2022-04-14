2c5441259be874fd6c2c0eb2f6a1845c6832ca81
ZhangzihanGit <zhangzihan_96@outlook.com>
2022-04-14 10:18:40 +0000

    update shell

--- run_evaluate.sh
+++ run_evaluate.sh
@@ -1,23 +1,7 @@
 DATASET='bbc'
-# DATASET='m10'
-# DATASET='20ng'
-
-# TOPIC_MODEL='lda'
-# TOPIC_MODEL='prodlda'
-# TOPIC_MODEL='zeroshottm'
-# TOPIC_MODEL='combinedtm'
 TOPIC_MODEL='cetopic'
-# TOPIC_MODEL='bertopic'
-
 WORD_SELECT_METHOD='tfidf_idfi'
-# WORD_SELECT_METHOD='tfidf_tfi'
-# WORD_SELECT_METHOD='tfidfi'
-# WORD_SELECT_METHOD='tfi'
-
-# PRETRAINED_MODEL='sentence-transformers/stsb-bert-base'
-PRETRAINED_MODEL='bert-base-uncased'
-# PRETRAINED_MODEL='princeton-nlp/unsup-simcse-bert-base-uncased'
-# PRETRAINED_MODEL='princeton-nlp/sup-simcse-roberta-base'
+PRETRAINED_MODEL='princeton-nlp/unsup-simcse-bert-base-uncased'
 
 
 python main.py\
